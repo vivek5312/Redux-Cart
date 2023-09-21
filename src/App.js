0edc6b1dd78c73daede1277aa435591358f9ecd5
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-09-21 12:15:17 +0530

    do htttp request

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import './App.css';
+import { useState, useEffect } from 'react';
+import { auth } from './Components/Firebase/Firebase';
+import Home from './Components/Home/Home';
+import Login from './Components/Auth/Login/Login';
+import Signup from './Components/Auth/Signup/Signup';
+import Profile from './Components/Profile/Profile';
+import ResetPassword from './Components/Auth/Fortgot/ResetPassword';
 
 function App() {
-  const isCartVisible = useSelector((state) => state.cart.isCartVisible);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
-    <Layout>
-      {isCartVisible &&<Cart />}
-      <Products />
-    </Layout>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={user ? <Navigate to="/home" /> : <Login />} />
+          <Route path="/signup" element={user ? <Navigate to="/home" /> : <Signup />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={ <Profile /> } />
+          <Route path="/reset" element={<ResetPassword />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 

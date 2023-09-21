69c518feb4fcb828e233f1f46b30df00761c52bc
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-09-21 12:19:34 +0530

    do https request using notification

--- src/App.js
+++ src/App.js
@@ -1,39 +1,70 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import './App.css';
-import { useState, useEffect } from 'react';
-import { auth } from './Components/Firebase/Firebase';
-import Home from './Components/Home/Home';
-import Login from './Components/Auth/Login/Login';
-import Signup from './Components/Auth/Signup/Signup';
-import Profile from './Components/Profile/Profile';
-import ResetPassword from './Components/Auth/Fortgot/ResetPassword';
+import Cart from "./components/Cart/Cart";
+import { useEffect } from "react";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector, useDispatch } from "react-redux";
+import Notification from "./components/UI/Notification";
+import { uiAction } from "./components/Store/index";
 
+let isIntial=true;
 function App() {
-  const [user, setUser] = useState(null);
+  const isCartVisible = useSelector((state) => state.cart.isCartVisible);
+  const cart = useSelector((state) => state.AddCart);
+  const dispatch = useDispatch();
+  const notification=useSelector(state=>state.cart.notification);
+
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setUser(user);
-    });
-
-    return () => {
-      unsubscribe();
+    const cartdata = async () => {
+      dispatch(
+        uiAction.notification({
+          status: "Pending",
+          title: "Sending",
+          message: "Send Cart data",
+        })
+      );
+      const response = await fetch(
+        "https://project-01-6659a-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("sending cart data is failed");
+      }
+      dispatch(
+        uiAction.notification({
+          status: "success",
+          title: "Success",
+          message: "Send Cart data succesfully",
+        }))
     };
-  }, []);
+   if(isIntial){
+    isIntial=false;
+   }
+    cartdata().catch((error)=>{
+      dispatch(
+        uiAction.notification({
+          status: "error",
+          title: "Error",
+          message: "Send Cart data failed",
+        }))
+    })
+     
+  }, [cart,dispatch]);
 
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={user ? <Navigate to="/home" /> : <Login />} />
-          <Route path="/signup" element={user ? <Navigate to="/home" /> : <Signup />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile" element={ <Profile /> } />
-          <Route path="/reset" element={<ResetPassword />} />
-        </Routes>
-      </Router>
-    </div>
+    <>
+    {notification && <Notification status={notification.status} title={notification.title} message={notification.message}/>}
+     <Layout>
+      {isCartVisible && <Cart />}
+      <Products />
+    </Layout>
+    </>
   );
+    
+   
 }
 
 export default App;

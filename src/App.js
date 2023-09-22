fe4d57a28ba34ad7b9a146c129754173c2310e6a
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-09-22 21:59:33 +0530

    fetch data

--- src/App.js
+++ src/App.js
@@ -4,55 +4,31 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector, useDispatch } from "react-redux";
 import Notification from "./components/UI/Notification";
-import { uiAction } from "./components/Store/index";
-
+import { sendCartData,fetchCartData} from "./components/Store/cartAction";
 let isIntial=true;
 function App() {
   const isCartVisible = useSelector((state) => state.cart.isCartVisible);
   const cart = useSelector((state) => state.AddCart);
   const dispatch = useDispatch();
   const notification=useSelector(state=>state.cart.notification);
-
+  
+  
+ 
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
   useEffect(() => {
-    const cartdata = async () => {
-      dispatch(
-        uiAction.notification({
-          status: "Pending",
-          title: "Sending",
-          message: "Send Cart data",
-        })
-      );
-      const response = await fetch(
-        "https://project-01-6659a-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("sending cart data is failed");
-      }
-      dispatch(
-        uiAction.notification({
-          status: "success",
-          title: "Success",
-          message: "Send Cart data succesfully",
-        }))
-    };
-   if(isIntial){
-    isIntial=false;
-   }
-    cartdata().catch((error)=>{
-      dispatch(
-        uiAction.notification({
-          status: "error",
-          title: "Error",
-          message: "Send Cart data failed",
-        }))
-    })
-     
-  }, [cart,dispatch]);
+    if (isIntial) {
+      isIntial = false;
+      return;
+    }
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
+
 
   return (
     <>
